6a3485558308c547616911d328548990aca439ae
GaminGravy <gravygamin69@gmail.com>
2025-02-08 20:13:57 -0800

    First commit

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1152,3 +1152,7 @@ EventScript_VsSeekerChargingDone::
 	.include "data/scripts/follower.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/Victory_Cavern/scripts.inc"
+
+	.include "data/maps/Fake_House/scripts.inc"
